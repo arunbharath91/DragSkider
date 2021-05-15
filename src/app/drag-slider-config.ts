949799fee99122839d99c0978ffebe6888d12cf5
Arun Bharath <arunbharath91@gmail.com>
2021-05-15 22:16:32 +0530

    init

---
+++ src/app/drag-slider-config.ts
@@ -0,0 +1,5 @@
+import { DragSlider } from "./drag-slider";
+
+const dragslider = new DragSlider('#drag-slider', { gap: 12, transition: true});
+
+const dragslider2 = new DragSlider('#drag-slider-chip', { threshold: 1, gap: 20, transition: false});
---
+++ src/app/main.ts
@@ -0,0 +1,2 @@
+import '../style.scss';
+import "./drag-slider-config";
