03235c4fe1f62a4daf4c69f8be2466e27ef222e6
Arun Bharath <arunbharath91@gmail.com>
2021-08-04 23:31:48 +0530

    update webpack5

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const WebpackMd5Hash = require("webpack-md5-hash");
 module.exports = {
   entry: ['./src/app/main'],
   devtool: 'inline-source-map',
@@ -18,9 +16,7 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          process.env.NODE_ENV !== "production"
-            ? "style-loader"
-            : MiniCssExtractPlugin.loader,
+          "style-loader",
           // Translates CSS into CommonJS
           {
             loader: "css-loader",
@@ -52,11 +48,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: "style.[chunkhash].css"
-    }),
     new HtmlWebpackPlugin({ // Also generate a test.html
       filename: 'index.html',
       template: 'src/index.html',
@@ -66,11 +57,15 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: 'src/assets', 
+          from: 'src/assets',
           to: 'assets',
+          noErrorOnMissing: true,
           globOptions: {
-            ignore: ['**/*.scss'],
-          }
+            ignore: [
+              // Ignore all `txt` files
+              "**/*.scss"
+            ],
+          },
         },
 
       ]
