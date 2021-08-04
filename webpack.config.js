20f3b6cd09bda19c534a469e93f35f1f69a22b4c
Arun Bharath <arunbharath91@gmail.com>
2021-08-04 20:56:24 +0530

    update --webpack5--

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,61 @@
 const path = require('path');
-const {
-  CleanWebpackPlugin
-} = require('clean-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const WebpackMd5Hash = require("webpack-md5-hash");
 module.exports = {
   entry: ['./src/app/main'],
   devtool: 'inline-source-map',
   module: {
-    rules: [{
-      test: /\.ts$/,
-      use: 'ts-loader',
-      exclude: [/node_modules\/rxjs/],
-    }, {
-      test: /\.scss$/,
-      use: [
-        "style-loader",
-        MiniCssExtractPlugin.loader,
-        "css-loader",
-        "sass-loader"
-      ]
-    }],
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          process.env.NODE_ENV !== "production"
+            ? "style-loader"
+            : MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          // Compiles Sass to CSS
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+              implementation: require("sass"),
+              sassOptions: {
+                fiber: false,
+                outputStyle: "compressed",
+              },
+            },
+          }
+        ],
+      }
+    ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "style.css"
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "style.[chunkhash].css"
     }),
     new HtmlWebpackPlugin({ // Also generate a test.html
       filename: 'index.html',
@@ -37,22 +63,17 @@ module.exports = {
       hash: true,
     }),
     new CleanWebpackPlugin(),
-    new CopyPlugin([{
-        from: 'src/assets',
-        to: 'assets',
-        ignore: ['*.scss'],
-      },
-      {
-        from: 'src/*.html',
-        to: '',
-        flatten: true,
-        ignore: ['index.html']
-      }
-    ]),
-    new WebpackMd5Hash()
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  }
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/assets', 
+          to: 'assets',
+          globOptions: {
+            ignore: ['**/*.scss'],
+          }
+        },
+
+      ]
+    })
+  ]
 };
